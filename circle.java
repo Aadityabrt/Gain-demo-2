a36dcec9bfdcd6ac6de5a24178bed5809ed6bc1f
Aaditya Jupally <aadi.jup@gmail.com>
2023-08-16 17:51:48 +0530

    changed area to pi*r*r*r

--- circle.java
+++ circle.java
@@ -1,3 +1,3 @@
 public static void main(String[] args){
-	Syste.out.println("The Area of the circle is Pi*r*r where r is the radius of the circle");
+	Syste.out.println("The Area of the circle is Pi*r*r*r where r is the radius of the circle");
 }
